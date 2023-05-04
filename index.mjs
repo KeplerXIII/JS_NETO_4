7ac6b818e04886ea9d8a0fef6cf119775efdb649
a.pushkin <a.v.pushkin@gmail.com>
2023-05-04 11:52:07 +0300

    rework

--- index.mjs
+++ index.mjs
@@ -1,60 +1,60 @@
-import readline from 'readline';
-import fs from 'fs/promises';
+import * as readlineSync from 'readline-sync';
+import fs from 'fs';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-const secretNumber = Math.floor(Math.random() * 100) + 1;
-let numberOfGuesses = 0;
+async function getUserInput() {
+  return new Promise(resolve => {
+      const userResponse = readlineSync.question('Введите число от 1 до 100: ');
+      resolve(userResponse);
+    });
+}
 
-const logGameStart = async () => {
-  try {
-    await fs.appendFile('game.log', `\n*********\nНовая игра. Загадано: ${secretNumber}\n`);
-  } catch (err) {
-        console.error(err);
+async function guessNumber() {
+  const secretNumber = random(1, 100);
+  let attempts = 0;
+  let done = false;
+  let message =`*****\nИгра началась. Загаданное число ${secretNumber}`;
+
+  const fileStream = fs.createWriteStream('./user-answers.log', {flags: 'a'});
+  
+  fs.closeSync(fs.openSync('./user-answers.log', 'a'));
+  fileStream.write(`${message}\n`);
+
+  while (!done) {
+    const input = await getUserInput();
+
+    fileStream.write(`Пользователь: ${input}\n`);
+
+    if (isNaN(input)) {
+      message = `Вы ввели не число. Попробуйте снова.`
+      fileStream.write(`Система: ${message}\n`);
+      console.log("Вы ввели не число. Попробуйте снова.");
+      continue;
+    }
+
+    attempts++;
+
+    if (input < secretNumber) {
+      message = `Загаданное число больше. Попыток: ${attempts}`
+      fileStream.write(`Система: ${message}\n`);
+      console.log(message);
+    } else if (input > secretNumber) {
+      message = `Загаданное число меньше. Попыток: ${attempts}`
+      fileStream.write(`Система: ${message}\n`);
+      console.log(message);
+    } else {
+      done = true;
+    }
   }
-};
+  message = `Вы угадали число "${secretNumber}" за ${attempts} попыток`
+  fileStream.write(`Система: ${message}\n`);
+  console.log(message);
+  fileStream.end();
+}
 
-const logGameResult = async (answer) => {
-  const message = (Number(answer) === secretNumber) ?
-   `Пользователь ввёл ${answer} и угадал число. Всего попыток: ${numberOfGuesses}\n` :
-   `Пользователь ввёл: ${answer}\n${answer < secretNumber ? 'Загаданное число больше.' : 'Загаданное число меньше.'} Попыток сделано: ${numberOfGuesses}\n`
-  try {
-    await fs.appendFile('game.log', message);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const game = async () => {
-  let answer;
-  try {
-    answer = await new Promise(resolve => rl.question('Угадайте число от 1 до 100: ', resolve)); // Обёртка промиса над коллбэком
-  } catch (err) {
-    console.error(err);
-  }
-  numberOfGuesses++;
-
-  if (Number(answer) === secretNumber) {
-    console.log(`Поздравляем, вы угадали число ${secretNumber} за ${numberOfGuesses} попыток!`);
-    await logGameResult(answer);
-    rl.close();
-    return;
-  }
-
-  if (Number(answer) < secretNumber) {
-    console.log(`Загаданное число больше. Попыток сделано:${numberOfGuesses}`);
-  } else {
-    console.log(`Загаданное число меньше. Попыток сделано:${numberOfGuesses}`);
-  }
-
-  await logGameResult(answer);
-  game();
-};
-
-(async () => {
-  await logGameStart();
-  await game();
-})();
+// guessNumber();
+await getUserInput();
+console.log('1')
